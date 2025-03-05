c715b9dc976f474b315b0f435367cbab4ea4d669
NIK MUHAMMAD ASYRAF <nikasyraf2001@gmail.com>
2025-03-05 15:14:26 +0800

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Welcome to Your Inertia-Vue App</h1>
+        <inertia-link href="/tasks">View Tasks</inertia-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
